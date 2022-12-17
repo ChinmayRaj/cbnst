a0923fe2dec883ec2e7ebc5c6289cd3466e5a8af
ChinmayRaj <100818920+ChinmayRaj@users.noreply.github.com>
2022-12-17 08:57:30 +0530

    Create simpson's 3by 8.c

---
+++ simpson's 1/simpson's 3by 8.c
@@ -0,0 +1,40 @@
+ #include<stdio.h>
+   #include<math.h>
+
+   float func(float x)
+   {
+   return 1 / ( 1 + x * x );
+   }
+
+   void main()
+   {
+printf(“Name: CHINMAY RAJ SHAH\n Section: H\n Class roll number: 50\n University roll  number: 2016711\n ”);
+    int n,i;
+    float a,b,h,result,sum=0,sum1=0;
+printf("Enter the lower limit: ");
+scanf("%f",&a);
+printf("Enter the upper limit: ");
+scanf("%f",&b);
+printf("Enter the interval: ");
+scanf("%d",&n);
+    float x[n],y[n];
+    h=(b-a)/(float)n;
+x[0]=a;
+   y[0]=func(a);
+   x[n]=b;
+   y[n]=func(b);
+for(int i=1;i<n;i++)
+   {
+       x[i]=x[0]+i*h;
+       y[i]=func(x[i]);
+   }
+for(int i=1;i<n;i++)
+   {
+       if(i%3==0)
+       sum+=y[i];
+       else
+       sum1+=y[i];
+   }
+    result=3*h/8*((y[0]+y[n])+3*sum1+2*sum);
+printf("Result: %lf",result);
+}
